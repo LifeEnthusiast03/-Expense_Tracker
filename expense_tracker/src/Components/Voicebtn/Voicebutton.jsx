6c72b542aebd529e625372b46e0c1d53caed7760
SOUGATA SAHA <sahasougata820@gmail.com>
2025-03-09 10:54:24 +0530

    random add

--- expense_tracker/src/Components/Voicebtn/Voicebutton.jsx
+++ expense_tracker/src/Components/Voicebtn/Voicebutton.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react";
 import { startVoiceRecognition, sendToWitAI } from "../../useVoice";
 import styles from "./style"; // Import the styles
+import {useContext} from "react"
+import { expenseTrackerContex } from '../../Context/Context'
 
 export default function Voicebutton() {
     const witAccessToken = "ZSBSXUG25Q3ZCRVZAYPOB6MG3KEXP4YL";
+    const intialstate={
+        amount:"",
+        category:"",
+        type:"",
+        date:new Date()
+      }
+    const [state,setstate]=useState(intialstate);
+    const {addTransaction}=useContext(expenseTrackerContex);
     const [transcript, setTranscript] = useState("Click the button and speak...");
     const handleSpeechDetected = async (spokenText) => {
-        const witResponse = await sendToWitAI(spokenText, witAccessToken);
-        console.log("Processed by Wit.ai:", witResponse);
-        // Extracting intent
-    const intent = witResponse?.intents?.[0]?.name;
-    console.log("Intent:", intent); // Example: "add_expense"
+        const data = await sendToWitAI(spokenText, witAccessToken);
+        console.log("Processed by Wit.ai:", data);
+        const type =data.entities["Type:Type"]?.[0]?.value; // "income"
+        const amount =data.entities["amount:amount"]?.[0]?.value; // "50"
+        const category = data.entities["catagory:catagory"]?.[0]?.value; // "Bill"
 
-    // Extracting amount
-    const amount = witResponse?.entities?.amount?.[0]?.value;
-    console.log("Amount:", amount); // Example: 500
+        console.log("Type:", type);
+        console.log("Amount:", amount);
+        console.log("Category:", category);
 
-    // Extracting category
-    const category = witResponse?.entities?.category?.[0]?.value;
-    console.log("Category:", category); // Example: "food"
-        
+
     };
 
 
